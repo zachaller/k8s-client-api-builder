fbd8ab38e4cd2f34836e8c974b3739b2723b2e66
Zach Aller <zach_aller@intuit.com>
2025-11-19 02:54:41 -0800

    save point
    Signed-off-by: Zach Aller <zach_aller@intuit.com>

--- examples/my-platform/api/v1alpha1/web_service_types.go
+++ examples/my-platform/api/v1alpha1/web_service_types.go
@@ -9,22 +9,22 @@ type WebServiceSpec struct {
 	// Image is the container image to deploy
 	// +kubebuilder:validation:MinLength=1
 	Image string `json:"image"`
-	
+
 	// Replicas is the number of pod replicas
 	// +kubebuilder:validation:Minimum=1
 	// +kubebuilder:validation:Maximum=100
 	// +kubebuilder:default=1
 	Replicas int32 `json:"replicas,omitempty"`
-	
+
 	// Port is the container port to expose
 	// +kubebuilder:validation:Minimum=1
 	// +kubebuilder:validation:Maximum=65535
 	Port int32 `json:"port"`
-	
+
 	// EnableIngress enables ingress creation
 	// +kubebuilder:default=false
 	EnableIngress bool `json:"enableIngress,omitempty"`
-	
+
 	// Domain is the domain for the ingress (required if enableIngress is true)
 	// +optional
 	Domain string `json:"domain,omitempty"`
@@ -55,8 +55,4 @@ type WebServiceList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []WebService `json:"items"`
-}
-
-func init() {
-	SchemeBuilder.Register(&WebService{}, &WebServiceList{})
 }
